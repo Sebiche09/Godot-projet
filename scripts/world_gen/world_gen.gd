b364444e6cf3d84115999d212fefc59ed30d42d6
ZADINOX <tom.membre1@gmail.com>
2024-01-21 01:01:44 +0100

    Mise a jour
    +

--- scripts/world_gen/world_gen.gd
+++ scripts/world_gen/world_gen.gd
@@ -21,39 +21,40 @@ func _ready():
 	for x in width:
 		var noise_height = int(noise.get_noise_1d(x) * 20)
 		dirt_height = randi_range(7,8)
-		
-			
+
+
+
 		#Remplir de grass
-		for y in dirt_height:
+		for y in range(0,dirt_height):
 			if y == 0:
 				proba = randi_range(0,20)
 				if proba == 0:
-					flower_fill_arr.append(Vector2(x, noise_height+y))
-			else:
+					flower_fill_arr.append(Vector2(x, noise_height-1))
+			else :
 				dirt_fill_arr.append(Vector2(x, noise_height+y))
 
 		BetterTerrain.set_cell(tile_map,0, Vector2(x, noise_height), 0)
-		
+
 		#Remplir de stone
 		for y in range(dirt_height, stone_height): 
 			if (noise_height+y) <= bedrock:
 				stone_fill_arr.append(Vector2(x, noise_height+y))
 			elif (noise_height+y) == bedrock:
 					bedrock_fill_arr.append(Vector2(x, noise_height+y))
-					
-					
+
+
 		BetterTerrain.set_cell(tile_map,0, Vector2(x, noise_height), 0)
-	
+
 
 
 	#Remplir la hauteur
 	BetterTerrain.set_cells(tile_map,0, height_tile_arr, 0)
 	BetterTerrain.update_terrain_cells(tile_map, 0, height_tile_arr,true)
-	
+
 	#Remplir de terre
 	BetterTerrain.set_cells(tile_map,0, dirt_fill_arr, 0)
 	BetterTerrain.update_terrain_cells(tile_map, 0, dirt_fill_arr,true)
-	
+
 	#Remplir de fleur
 	BetterTerrain.set_cells(tile_map,0, flower_fill_arr, 3)
 	BetterTerrain.update_terrain_cells(tile_map, 0, flower_fill_arr,true)
@@ -61,7 +62,7 @@ func _ready():
 	#Remplir de stone
 	BetterTerrain.set_cells(tile_map,0, stone_fill_arr, 1)
 	BetterTerrain.update_terrain_cells(tile_map, 0, stone_fill_arr,true)
-	
+
 	#Remplir de bedrock
 	BetterTerrain.set_cells(tile_map,0, bedrock_fill_arr, 2)
 	BetterTerrain.update_terrain_cells(tile_map, 0, bedrock_fill_arr,true)
